ff47538dbf395f8090f820490fc01ef62d9014ef
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2022-12-20 15:29:40 +0200

    precorrection commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+(() => {
+  const galleryEl = document.querySelector(".gallery");
+
+  const galleryItemsHTMLString = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+					<a class="gallery__link" href="${original}">
+						<img class="gallery__image" src="${preview}" alt="${description}" loading="lazy" data-source="${original}" />
+					</a>
+				</li>`
+    )
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", galleryItemsHTMLString);
+
+  const onGalleryItemClick = (event) => {
+    event.preventDefault();
+    const { target } = event;
+    if (target.nodeName !== "IMG") return;
+
+    //creating basicLightbox instance
+    const instance = basicLightbox.create(`
+      <div class="modal">
+          <img src="${target.dataset.source}" alt="${target.alt}" />
+      </div>
+    `);
+    instance.show();
+
+    //when modal window is open adding Ecs keydown event listener on document
+    const onEscKeydown = (event) => {
+      //checking if Esc pressed
+      if (event.code !== "Escape") return;
+      //if Esc is pressed closing modal window and removing Esc keydown event listener from document
+      instance.close();
+      document.removeEventListener("keydown", onEscKeydown);
+    };
+    document.addEventListener("keydown", onEscKeydown);
+  };
+
+  galleryEl.addEventListener("click", onGalleryItemClick);
+})();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+(() => {
+  const galleryEl = document.querySelector(".gallery");
+  const galleryItemsHTMLString = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+ 				 	<img class="gallery__image" src="${preview}" alt="${description}" />
+			 	</a>`
+    )
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", galleryItemsHTMLString);
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+})();
